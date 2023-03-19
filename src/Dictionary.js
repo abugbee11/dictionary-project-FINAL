16751bec49d4024664c566f3c6cc0300a514c0cb
abugbee11 <abugbee11@gmail.com>
2023-03-19 16:35:34 -0400

    Rendered the Meaning component and redid original steps on new file on new PC

---
+++ src/Dictionary.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Dictionary.css";
+import Results from "./Results";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setResults(response.data);
+  }
+
+  function search(event) {
+    event.preventDefault();
+
+    //documentation: https://www.shecodes.io/learn/apis/dictionary
+
+    let apiKey = "0b3e84o47fbcat613600bf4527c3e4e0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
+}
