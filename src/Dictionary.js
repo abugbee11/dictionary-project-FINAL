7cd636c922e1e8d9ac815e2fc82c272d29fe1e29
abugbee11 <abugbee11@gmail.com>
2023-03-20 15:05:03 -0400

    Added SheCodes Photos API feature and removed unnecessary console logs

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
     setResults(response.data);
+  }
+
+  function handleShecodesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -19,6 +24,10 @@ export default function Dictionary(props) {
     let apiKey = "0b3e84o47fbcat613600bf4527c3e4e0";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let shecodesApiKey = "0b3e84o47fbcat613600bf4527c3e4e0";
+    let shecodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${shecodesApiKey}`;
+    axios.get(shecodesApiUrl).then(handleShecodesResponse);
   }
 
   function handleSubmit(event) {
@@ -53,6 +62,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
